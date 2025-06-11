08a6c6fb2c66b13726cb2ec86d64d97ab6da0e88
Luis Fernando <luispv.1002@gmail.com>
2025-06-11 16:31:53 -0600

    ajustes lista productos

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,7 +1,8 @@
-export const getDataFetch = async () => {
 
-    const response = await fetch(`http://localhost:5000/products`);
-    const data = await response.json();
+import products from "./products";  
+
+export const getDataFetch = async () => {
+    const data = products;
         
     return data
     
